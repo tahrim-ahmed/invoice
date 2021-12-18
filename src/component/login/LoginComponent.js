593eddea000bb54798edf31f0dc974234fb74a70
Tahrim Ahmed <tamiad1618@gmail.com>
2021-12-18 23:35:03 +0600

    -login form created

--- src/component/login/LoginComponent.js
+++ src/component/login/LoginComponent.js
@@ -2,9 +2,21 @@ import * as React from "react";
 
 const LoginComponent = () => {
     return(
-        <div>
-            Form
-        </div>
+        <form action="">
+            <div>
+                <label htmlFor="username">
+                    Username
+                </label>
+                <input type="text" name="username" id="username"/>
+                <br/>
+                <label htmlFor="password">
+                    Password
+                </label>
+                <input type="password" name="password" id="password"/>
+                <br/>
+                <button type="submit">Login</button>
+            </div>
+        </form>
     )
 }
 
